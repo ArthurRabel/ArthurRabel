ebbd298e3ea721ce8a7a338b8a8f609bb2dc475b
Rabelo <Arthur.rabel@outlook.com>
2024-03-10 20:55:43 -0300

    footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -2,8 +2,8 @@ import './index.css';
 
 export default function footer() {
     return (
-        <footer>
-            
+        <footer className='c-footer'>
+            <p>&copy; 2024 Arthur da Mata Rabelo. Todos os direitos reservados.</p>
         </footer>
     )
 }
