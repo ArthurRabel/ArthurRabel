13dbe23e3ac511cf393d5ae04a7230f16321bea6
Rabelo <Arthur.rabel@outlook.com>
2024-03-26 18:07:06 -0300

    animation css

--- src/components/introduction/index.jsx
+++ src/components/introduction/index.jsx
@@ -7,9 +7,9 @@ export default function introduction() {
         <> 
             <section className='c-introduction'>
                 <div className='wrapper c-introduction__wrapper'>
-                    <h2>Olá, seja bem-vindo</h2>
-                    <h1>Meu nome é <span>Arthur</span>, um <span>desenvolvedor web</span></h1>
-                    <p>Ansioso para me juntar a uma equipe inovadora e contribuir para o sucesso de projetos empolgantes.</p>
+                    <h2 className='animation'>Olá, seja bem-vindo</h2>
+                    <h1 className='animation'>Meu nome é <span>Arthur</span>, um <span>desenvolvedor web</span></h1>
+                    <p className='animation'>Ansioso para me juntar a uma equipe inovadora e contribuir para o sucesso de projetos empolgantes.</p>
                 </div>
                 <Arrow />
             </section>
