11a16da7b588ebacf80734ae87e23f5307e322fd
Rabelo <Arthur.rabel@outlook.com>
2024-04-10 21:30:16 -0300

    update

--- src/components/introduction/index.jsx
+++ src/components/introduction/index.jsx
@@ -8,7 +8,7 @@ export default function introduction() {
             <section className='c-introduction'>
                 <div className='wrapper c-introduction__wrapper'>
                     <h2 className='animation'>Olá, seja bem-vindo</h2>
-                    <h1 className='animation'>Meu nome é <span>Arthur</span>, um <span>desenvolvedor web</span></h1>
+                    <h1 className='animation'>Meu nome é <span className='c-introduction__special_Text'>Arthur</span>, um <span>desenvolvedor web</span></h1>
                     <p className='animation'>Ansioso para me juntar a uma equipe inovadora e contribuir para o sucesso de projetos empolgantes.</p>
                 </div>
                 <Arrow />
